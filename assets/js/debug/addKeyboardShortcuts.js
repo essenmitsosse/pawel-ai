4a2fe98f47c453fcbca27f9441687367d1cad33b
marcus <marcus@essenmitsosse.de>
2016-06-22 03:44:44 +0200

    add Ctrl + A as a shortcut to not show animation

--- assets/js/debug/addKeyboardShortcuts.js
+++ assets/js/debug/addKeyboardShortcuts.js
@@ -1,7 +1,7 @@
 define( [ "helper/vars", "helper/globalSetter", "debug/keyboardHandler" ], function ( _vars, globalSetter, keyboardHandler ) {
 
 	// noAnimation
-	keyboardHandler.addKeyboardShortcut( { key: 71, ctrl: true, }, globalSetter.getGlobalToggler( "noAnimation" ) );
+	keyboardHandler.addKeyboardShortcut( { key: 65, ctrl: true, }, globalSetter.getGlobalToggler( "noAnimation" ) );
 	
 	// debug
 	keyboardHandler.addKeyboardShortcut( { key: 71, ctrl: true, }, globalSetter.getGlobalToggler( "grid" ) );
