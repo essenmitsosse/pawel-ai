c0d0f9b77c3677a0e98c9ab41f1ab169b53f3971
marcus <marcus@essenmitsosse.de>
2016-07-11 03:34:50 +0200

    add pause after header

--- assets/js/typewriter/classes/Header.js
+++ assets/js/typewriter/classes/Header.js
@@ -1,9 +1,9 @@
 define( [
 	"helper/const",
-	"typewriter/classes/BasicTypeElement",
-	"typewriter/classes/Image",
-	"helper/errorMessenger"
-	], function ( _const, BasicTypeElement, Image, errorMessenger ) {
+	"helper/globals",
+	"debug/keyboardHandler",
+	"typewriter/classes/BasicTypeElement"
+], function ( _const, _globals, keyboardHandler, BasicTypeElement ) {
 
 	function Header( args ) {
 		this.basicSetup( args );
@@ -17,6 +17,14 @@ define( [
 	// Header.prototype.possibleChildElements = [ { elementName: "img", ClassConstructor: Image } ];
 	Header.prototype.isElement = true;
 	Header.prototype.afterDelay = _const.titleDuration || 1500;
+
+	Header.prototype.finish = function () {
+		if ( _globals.pauseAfterChapter ) {
+			keyboardHandler.prevNextKeyboardShortcut.addNext( BasicTypeElement.prototype.finish.bind( this ) );
+		} else {
+			BasicTypeElement.prototype.finish.bind( this )();
+		}
+	};
 
 	return Header;
 } );
